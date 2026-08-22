ffeeb430b7b48751993e8bf6ff34671c752e3502
Eiphel <ch.hasanattasawar@gmail.com>
2026-08-22 13:10:26 +0500

    null

--- Data Extractor/ConsoleApplication1/ConsoleApplication1.cpp
+++ Data Extractor/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,35 @@
 #include<iostream>
 #include<math.h>
 #include<fstream>
+#include<string>
+#include<vector>
 
 using namespace std;
 
 class data_read
 {
 protected:
-	double spot,strike,vol,exp,risk,
+	double spot, strike, vol, exp, risk;
+	const double euler = 2.718;
+	int size = 0;
 public:
-	ifstream file{ "C:\\Users\\pc\\Documents\\GitHub\\Options-Pricing-Engine\\Data Extractor\\Data Extractor\\data_inputs.csv" };
+	vector<vector<string>>inputs;
+	void read_data()
+	{
+	ifstream data{ "C:\\Users\\pc\\Documents\\GitHub\\Options-Pricing-Engine\\Data Extractor\\Data Extractor\\data_inputs.csv" };
+	string line,temp;
+	getline(data, line);
+	getline(data, line);
+	size = line.find(",");
+	temp = line.substr(0,size);
+	cout << temp;
+	
+    }
 
 };
 int main()
 {
 	data_read d;
-	d.run();
+	d.read_data();
 }
 
