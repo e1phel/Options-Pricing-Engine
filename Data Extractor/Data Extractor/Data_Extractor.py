ee9558f2b38acb6f88d79eb24cb75a244a2e6a2e
Eiphel <ch.hasanattasawar@gmail.com>
2026-08-22 11:48:39 +0500

    Done
    Works!!

--- Data Extractor/Data Extractor/Data_Extractor.py
+++ Data Extractor/Data Extractor/Data_Extractor.py
@@ -1,11 +1,26 @@
+from os import write
 import yfinance as yf
 import numpy as nf
+import csv
 stock = yf.Ticker("NVDA")
 history = stock.history(period="1y")
 returns = history['Close'].pct_change().dropna()
 spot = history['Close'].iloc[-1]
 volatility = returns.std()*(252**0.5)
+risk_rate = 0.038
 exp = stock.options[0]
 call_option = stock.option_chain(exp)
-print(call_option)
+call = call_option.calls[['strike','lastPrice']]
+ 
+with open('market_inputs.csv', 'w', newline='') as f:
+      writer = csv.writer(f)
+      writer.writerow(['ticker', 'spot', 'volatility', 'rate', 'expiry'])
+      writer.writerow([stock, spot, returns, risk_rate, exp])
 
+call.to_csv('option_chain.csv', index=False)
+
+print(f"Ticker :{stock}")
+print(f"Spot :{spot}")
+print(f"Volatility :{volatility}")
+print(f"Risk-Free Rate :{risk_rate}")
+print(f"Expiry :{exp}")
